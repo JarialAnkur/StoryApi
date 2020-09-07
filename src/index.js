f3f0d58747b9201bc02c899d8739d1c1260d1972
Ankur Jarial <ankujarialthakur@gmail.com>
2020-09-07 13:52:37 +0530

    Build

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const express=require("express");
+const path=require("path");
+const cors=require("cors");
 const userRoute=require("./routes/user");
 const storyRoute=require("./routes/story");
 require("./db/mongoose");
 
+const path
 const app=express();
 const port=process.env.PORT;
+app.use(cors());
 app.use(express.json());
 app.use(userRoute);
 app.use(storyRoute);
 
-app.listen(port,()=>{
-    console.log("Server is running ");
-})
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('public'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '/public/index.html'));
+    });
+  }
+  
+  server.listen(port, () => {
+    console.log(`listening on PORT ${port}`)
+  })
---
+++ src/public/precache-manifest.d5c84417d80756718d2714f9b9af2e5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51982268d6133fadac7ab29e127be87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e4fa62fce952f606de2",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "64102582b0e7f5b40bdd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e4fa62fce952f606de2",
+    "url": "/static/js/2.d5e6b324.chunk.js"
+  },
+  {
+    "revision": "34e328df1cf782718d593f89571474d2",
+    "url": "/static/js/2.d5e6b324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64102582b0e7f5b40bdd",
+    "url": "/static/js/main.9831a0a3.chunk.js"
+  },
+  {
+    "revision": "c809e8724fc75d877dd0",
+    "url": "/static/js/runtime-main.526427e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
